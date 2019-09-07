6f851751d0a9f7711dafb2321de0de2f01622615
Carl Ortutay <carlorty@gmail.com>
2019-09-07 18:27:24 -0400

    formating and cleaing out console logs

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import List from "./List";
 import Cart from "./Cart";
 import "./App.css";
 
-// This object will allow us to
-// easily convert numbers into US dollar values
-/* const USCurrencyFormat = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-}); */
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -47,21 +40,6 @@ class App extends Component {
   };
 
   render() {
-   /*  const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className='summary__option' key={featureHash}>
-          <div className='summary__option__label'>{feature} </div>
-          <div className='summary__option__value'>{selectedOption.name}</div>
-          <div className='summary__option__cost'>
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    }); */
-
     const total = Object.keys(this.state.selected).reduce(
       (acc, curr) => acc + this.state.selected[curr].cost,
       0
@@ -73,26 +51,13 @@ class App extends Component {
           <h1>ELF Computing | Laptops</h1>
         </header>
         <main>
-            <List
-              features={this.props.features}
-              onChange={this.updateFeature}
-              stateChanger={this.state.selected}
-            />
-
-            <Cart 
+          <List
+            features={this.props.features}
+            onChange={this.updateFeature}
             stateChanger={this.state.selected}
-            total={total}
-            />
-          {/* <section className='main__summary'>
-            <h2>Your cart</h2>
-            {summary}
-            <div className='summary__total'>
-              <div className='summary__total__label'>Total</div>
-              <div className='summary__total__value'>
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section> */}
+          />
+
+          <Cart stateChanger={this.state.selected} total={total} />
         </main>
       </div>
     );
--- src/Items.js
+++ src/Items.js
@@ -8,7 +8,6 @@ const USCurrencyFormat = new Intl.NumberFormat("en-US", {
 
 class Items extends Component {
   render() {
-    console.log(this.props.stateChanger)
     return (
       <div key={this.props.featureHashkey} className='feature__item'>
         <input
@@ -16,15 +15,13 @@ class Items extends Component {
           id={this.props.itemHash}
           className='feature__option'
           name={slugify(this.props.name)}
-          checked={
-            this.props.name ===
-            this.props.stateChanger[this.props.name]
+          checked={this.props.name === this.props.stateChanger[this.props.name]}
+          onChange={e =>
+            this.props.onChange(this.props.featureName, this.props.item)
           }
-          onChange={e => this.props.onChange(this.props.featureName, this.props.item)}
         />
         <label htmlFor={this.props.itemHash} className='feature__label'>
-          {this.props.name} (
-          {USCurrencyFormat.format(this.props.cost)})
+          {this.props.name} ({USCurrencyFormat.format(this.props.cost)})
         </label>
       </div>
     );
